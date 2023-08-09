b774290312994812dc8712c154b4f67834e007ac
hoangnk <hoangkimzxc@gmail.com>
2023-08-09 10:01:52 +0700

    feature/user-is-typing-show-history-clear-history-when-pp-out

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
 
 function Chat({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
+  const [typingUsers, setTypingUsers] = useState([]);
 
   const sendMessage = async () => {
     if (currentMessage !== "") {
@@ -26,13 +27,53 @@ function Chat({ socket, username, room }) {
     socket.on("receive_message", (data) => {
       setMessageList((list) => [...list, data]);
     });
-    return () => socket.removeListener("receive_message");
-  }, [socket]);
+
+    socket.on("typing", (data) => {
+      if (data.room === room) {
+        setTypingUsers((users) => {
+          if (!users.includes(data.username)) {
+            return [...users, data.username];
+          }
+          return users;
+        });
+
+        setTimeout(() => {
+          setTypingUsers((users) =>
+            users.filter((user) => user !== data.username)
+          );
+        }, 2000);
+      }
+    });
+
+    socket.on("load_old_messages", (oldMessages) => {
+      setMessageList(oldMessages);
+    });
+
+    socket.on("clear_chat_history", () => {
+      setMessageList([]); // Clear chat history
+    });
+
+    // Request old messages after socket connection is established
+    socket.on("connect", () => {
+      socket.emit("join_room", room);
+    });
+
+    return () => {
+      socket.removeListener("receive_message");
+      socket.removeListener("typing");
+      socket.removeListener("load_old_messages");
+      socket.removeListener("connect");
+    };
+  }, [socket, room]);
+
+  const handleInputChange = () => {
+    socket.emit("typing", { username, room });
+  };
 
   return (
     <div className="chat-window">
       <div className="chat-header">
-        <p>Live Chat</p>
+        <p>Username: {username}</p>
       </div>
       <div className="chat-body">
         <ScrollToBottom className="message-container">
@@ -54,6 +95,12 @@ function Chat({ socket, username, room }) {
               </div>
             </div>
           ))}
+          {typingUsers.length > 0 && (
+            <div className="typing-indicator">
+              {typingUsers.join(", ")} {typingUsers.length > 1 ? "are" : "is"}{" "}
+              typing...
+            </div>
+          )}
         </ScrollToBottom>
       </div>
       <div className="chat-footer">
@@ -61,12 +108,23 @@ function Chat({ socket, username, room }) {
           type="text"
           placeholder="Hey..."
           value={currentMessage}
-          onChange={(e) => setCurrentMessage(e.target.value)}
+          onChange={(e) => {
+            setCurrentMessage(e.target.value);
+            handleInputChange();
+          }}
           onKeyPress={(e) => {
-            event.key === "Enter" && sendMessage();
+            if (e.key === "Enter") {
+              sendMessage();
+            }
           }}
         />
-        <button onClick={sendMessage}>&#9658;</button>
+        <button
+          onClick={() => {
+            sendMessage();
+          }}
+        >
+          &#9658;
+        </button>
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,38 +1,86 @@
-const express = require('express')
-const app = express()
-const http = require('http')
-const cors = require('cors')
-const { Server } = require('socket.io')
-app.use(cors())
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
 
-const server = http.createServer(app)
+app.use(cors());
+
+const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
-        methods: ["GET", "POST"]
+        origin: "*",
+        methods: ["GET", "POST"],
+        "Referrer Policy": "strict-origin-when-cross-origin"
     }
-})
+});
+
+// Store users in rooms
+const roomUsers = {};
+
+// Store old messages in a data structure
+const roomMessages = {};
 
 io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
 
     socket.on("join_room", (data) => {
-        socket.join(data)
+        socket.join(data);
         console.log(`User with ID: ${socket.id} join room: ${data}`);
-    })
 
+        // Send old messages for the joined room
+        if (roomMessages[data]) {
+            socket.emit("load_old_messages", roomMessages[data]);
+        }
+
+        // Store user in room
+        if (!roomUsers[data]) {
+            roomUsers[data] = [];
+        }
+        roomUsers[data].push(socket.id);
+    });
+
+
+
+    socket.on('typing', data => {
+        socket.broadcast.emit('typing', data);
+    });
 
     socket.on("send_message", (data) => {
-        socket.to(data.room).emit('receive_message', data)
-        console.log(data)
-    })
+        socket.to(data.room).emit('receive_message', data);
+        console.log(data);
+
+        // Store the message in the roomMessages data structure
+        if (!roomMessages[data.room]) {
+            roomMessages[data.room] = [];
+        }
+        roomMessages[data.room].push(data);
+    });
 
     socket.on("disconnect", () => {
         console.log("User Disconnected", socket.id);
-    })
-})
+        // Remove user from room
+        for (const room in roomUsers) {
+            const index = roomUsers[room].indexOf(socket.id);
+            if (index !== -1) {
+                roomUsers[room].splice(index, 1);
+
+                // Clear chat history if everyone left the room
+                if (roomUsers[room].length === 0) {
+                    delete roomMessages[room];
+                    io.to(room).emit("clear_chat_history");
+                }
+
+                break;
+            }
+        }
+
+    });
+
+
+});
 
 server.listen(3001, () => {
     console.log('SERVER RUNNING');
-})
+});
